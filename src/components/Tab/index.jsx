b065e3b1e0d5ef4fe562ed78313c982c432a62c3
Kyubiful <ser.zabala@gmail.com>
2022-04-09 22:49:24 +0200

    ✨ feat: create custom active link for menu

--- src/components/Tab/index.jsx
+++ src/components/Tab/index.jsx
@@ -1,11 +1,16 @@
-import { NavLink } from 'react-router-dom'
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 
-export const Tab = ({ to, children }) => {
+export const Tab = ({ to, children, ...props }) => {
+  const resolved = useResolvedPath(to)
+  const match = useMatch({ path: resolved.pathname, end: true })
+
   return (
-    <li className="">
-      <NavLink to={ to } className={({ isActive }) => isActive ? 'bg-slate-500 px-6 py-2' : 'bg-slate-200'}>
-        {children}
-      </NavLink>
+    <li className={ match ? 'border-r border-slate-700 ' : 'border-r border-slate-700 shadow-[inset_0_-1px_0_#334155]'}>
+      <div className={ match ? 'bg-gradient-to-r from-pink-400 to-cyan-300 pb-1' : 'bg-gradient-to-r from-pink-400 to-cyan-300 mb-1'}>
+        <Link {...props} to={to} className={ match ? 'bg-slate-700 px-6 py-2 h-2 flex justify-center items-center w-auto h-full' : 'bg-slate-900 px-6 py-2 h-2 flex justify-center items-center h-full w-full'}>
+          {children}
+        </Link>
+      </div>
     </li>
   )
 }
