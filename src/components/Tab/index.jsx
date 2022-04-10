32a2bfad8453abf6c3101ccd58dbe546b49a6e7b
Kyubiful <ser.zabala@gmail.com>
2022-04-10 22:15:28 +0200

    ✨ feat: change component style

--- src/components/Tab/index.jsx
+++ src/components/Tab/index.jsx
@@ -5,9 +5,9 @@ export const Tab = ({ to, children, ...props }) => {
   const match = useMatch({ path: resolved.pathname, end: true })
 
   return (
-    <li className={ match ? 'border-r border-slate-700 ' : 'border-r border-slate-700 shadow-[inset_0_-1px_0_#334155]'}>
-      <div className={ match ? 'bg-gradient-to-r from-pink-400 to-cyan-300 pb-1' : 'bg-gradient-to-r from-pink-400 to-cyan-300 mb-1'}>
-        <Link {...props} to={to} className={ match ? 'bg-slate-700 px-6 py-2 h-2 flex justify-center items-center w-auto h-full' : 'bg-slate-900 px-6 py-2 h-2 flex justify-center items-center h-full w-full'}>
+    <li className={ match ? 'border-r border-slate-700 ' : 'border-r border-slate-700 shadow-[inset_0_-1px_0_#334155] hover:bg-slate-500 transition'}>
+      <div className={ match ? 'bg-gradient-to-r from-pink-400 to-cyan-300 pb-1' : 'bg-gradient-to-r from-pink-400 to-cyan-300'}>
+        <Link {...props} to={to} className={ match ? 'text-sm bg-slate-700 px-3 py-1 flex justify-center items-center w-auto h-full' : 'text-sm bg-slate-900 px-3 py-1 flex justify-center items-center h-full w-full hover:bg-slate-500 transition'}>
           {children}
         </Link>
       </div>
