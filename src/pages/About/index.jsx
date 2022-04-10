a9e24cb214ae62e8a51866edd6aded0b7b23f951
Kyubiful <ser.zabala@gmail.com>
2022-04-10 22:19:15 +0200

    ✨ feat: add components to about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,40 @@
+import { AboutMe } from '../../components/AboutMe/index.jsx'
+import { Opinions } from '../../components/Opinions/index.jsx'
+import { Markup } from '../../components/Markup/index.jsx'
+
 export const About = () => {
   return (
-    <h1>Hello About</h1>
+    <div className="pt-16 pb-6 h-full flex flex-col">
+      <div className="h-40 flex flex-col justify-between">
+        <div>
+          <Markup text="<html>" className="pl-10" />
+          <Markup text="<body>" className="pl-12" />
+        </div>
+        <div>
+          <Markup text="<div>" className="pl-14" />
+        </div>
+      </div>
+      <div className="flex flex-col h-auto max-w-4xl w-full m-auto pr-6 pl-6">
+        <AboutMe />
+      </div>
+      <div className="flex flex-col justify-between">
+        <div>
+          <Markup text="</div>" className="pl-14" />
+          <Markup text="<div>" className="pl-14" />
+        </div>
+      </div>
+      <div className="flex flex-col h-auto max-w-4xl w-full m-auto pr-6 pl-6">
+        <Opinions />
+      </div>
+      <div className="h-40 flex flex-col justify-between">
+        <div>
+          <Markup text="</div>" className="pl-14" />
+        </div>
+        <div>
+          <Markup text="</body>" className="pl-12" />
+          <Markup text="</html>" className="pl-10" />
+        </div>
+      </div>
+    </div>
   )
 }
