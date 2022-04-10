7af9b924d368a1decf05858840ddd01b6ce720ae
Kyubiful <ser.zabala@gmail.com>
2022-04-10 22:11:18 +0200

    ✨ feat: create markup component

--- src/components/Markup/index.jsx
+++ src/components/Markup/index.jsx
@@ -1,10 +1,10 @@
 export const Markup = ({ text, className }) => {
   return (
     <div className="italic cursor-default select-none">
-      <p className={'absolute text-slate-200/10 ' + className}>
+      <p className={'absolute text-slate-200/30 font-extralight ' + className}>
         {text}
       </p>
-      <p className={'text-pink-500/30 blur-sm ' + className}>
+      <p className={'text-pink-500/40 blur-sm font-extralight ' + className}>
         {text}
       </p>
     </div>
