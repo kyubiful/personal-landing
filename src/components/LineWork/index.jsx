9b041325585111409a726b74f7ca81fc273c2035
Kyubiful <ser.zabala@gmail.com>
2022-04-12 17:51:32 +0200

    ✨ feat: create linework component

---
+++ src/components/LineWork/index.jsx
@@ -0,0 +1,15 @@
+export const LineWork = ({ className }) => {
+  return (
+    <svg className={className} viewBox="0 0 11 316" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="5" y1="1" x2="4.99999" y2="315" strokeWidth="2" strokeLinecap="round"/>
+      <circle cx="5" cy="110" r="5" />
+      <circle cx="5" cy="110" r="5" />
+      <circle cx="5" cy="29" r="5" />
+      <circle cx="5" cy="29" r="5" />
+      <circle cx="5" cy="191" r="5" />
+      <circle cx="5" cy="191" r="5" />
+      <circle cx="5" cy="272" r="5" />
+      <circle cx="5" cy="272" r="5" />
+    </svg>
+  )
+}
