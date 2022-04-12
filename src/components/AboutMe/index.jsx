d584895ade7657fc5d712891c9a96f70b2f844e9
Kyubiful <ser.zabala@gmail.com>
2022-04-12 17:54:11 +0200

    ✨ feat: add title component to aboutme component

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,10 +1,9 @@
+import { Title } from '../Title/index.jsx'
+
 export const AboutMe = () => {
   return (
     <>
-      <div className="flex items-center">
-        <h1 className="text-3xl min-w-max">Sobre mi</h1>
-        <div className="ml-2 h-0.5 max-w-xs w-full bg-slate-200"/>
-      </div>
+      <Title title="Sobre mi" />
       <div className="flex justify-between items-center">
         <div className="max-w-md w-full">
           <p>
