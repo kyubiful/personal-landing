f9f66b7675e1e15e32a58bfe88725a656897bb73
Kyubiful <ser.zabala@gmail.com>
2022-04-12 17:56:12 +0200

    ✨ feat: create array with opinions

--- src/components/Opinions/index.jsx
+++ src/components/Opinions/index.jsx
@@ -1,12 +1,40 @@
 import { OpinionCard } from '../OpinionCard/index.jsx'
 
+const OPINION_LIST = [
+  {
+    name: 'David Jimenez',
+    company: 'ESL Spain',
+    text: 'Sergio es una persona asdasd asdasd asdasdasda asdasdasd',
+    url: 'https://esl.gg'
+  },
+  {
+    name: 'Iván ...',
+    company: 'MistMeals',
+    text: 'Sergio es una persona asdasd asdasd asdasdasda asdasdasd',
+    url: 'https://mistmeals.com'
+  },
+  {
+    name: 'Héctor Ramón',
+    company: 'Play Generations',
+    text: 'Sergio es una persona asdasd asdasd asdasdasda asdasdasd',
+    url: 'https://playgenerations.com'
+  },
+  {
+    name: 'Otra Persona',
+    company: 'Otra compañía',
+    text: 'Sergio no es una persona asdasd asdasd asdasdasda asdasdasd',
+    url: 'https://google.es'
+  }
+]
+
 export const Opinions = () => {
   return (
-    <div className="flex gap-4">
-      <OpinionCard name="David Jimenez" company="ESL Spain" text="Sergio es una persona asdasd asdasd asdasdasda asdasdasd"/>
-      <OpinionCard name="Iván ..." company="MistMeals" text="Sergio es una persona asdasd asdasd asdasdasda asdasdasd"/>
-      <OpinionCard name="Héctor Ramón" company="Play Generations" text="Sergio es una persona asdasd asdasd asdasdasda asdasdasd"/>
-      <OpinionCard name="Otra Persona" company="Otra compañia" text="Sergio es una persona asdasd asdasd asdasdasda asdasdasd"/>
+    <div className="grid grid-cols-4 gap-3">
+      {
+        OPINION_LIST.map((opinion, index) => {
+          return <OpinionCard key={index} name={opinion.name} company={opinion.company} text={opinion.text}/>
+        })
+      }
     </div>
   )
 }
