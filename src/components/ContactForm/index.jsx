1adb1ae6bf6b276e30bbf01d9eaaafb87b47a864
Kyubiful <ser.zabala@gmail.com>
2022-05-10 19:27:44 +0200

    ✨ feat: reset form after send info

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -5,14 +5,17 @@ const onSubmit = (e) => {
   e.preventDefault()
   const data = Object.fromEntries(new FormData(e.target))
   console.log(data)
+  e.target.name.value = ''
+  e.target.email.value = ''
+  e.target.content.value = ''
 }
 
 export const ContactForm = () => {
   return (
     <form onSubmit={onSubmit} className="flex flex-col gap-2 mt-10">
-      <InputForm type="text" name="name" />
-      <InputForm type="email" name="email" />
-      <TextAreaForm name="content" />
+      <InputForm type="text" name="name" placeholder="Nombre" />
+      <InputForm type="email" name="email" placeholder="Email" />
+      <TextAreaForm name="content" placeholder="Texto" />
       <div className="flex justify-end">
         <InputForm type="submit" />
       </div>
