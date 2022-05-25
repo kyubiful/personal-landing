17bc9b0ef6df3c8ebf79ad720c33b36cbf5b5f79
Kyubiful <ser.zabala@gmail.com>
2022-05-25 21:55:22 +0200

    ✨ feat: add test dependences and config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test.config.js',
+    url: 'http://localhost:8080'
+  }
 })
