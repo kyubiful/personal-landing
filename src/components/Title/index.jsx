1a021e85bdeca008715c45bacc803a8470c69369
Kyubiful <ser.zabala@gmail.com>
2022-05-25 21:56:32 +0200

    ♻️ refactor: change name for title component input text

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,7 +1,7 @@
-export const Title = ({ title, className }) => {
+export const Title = ({ text }) => {
   return (
       <div className="flex items-center mt-5 mb-5 md:mt-auto lg:mt-auto">
-        <h1 className="text-lg md:text-3xl lg:text-3xl min-w-max">{title}</h1>
+        <h1 className="text-lg md:text-3xl lg:text-3xl min-w-max">{text}</h1>
         <div className="ml-2 h-0.5 max-w-xs w-full bg-slate-200"/>
       </div>
   )
