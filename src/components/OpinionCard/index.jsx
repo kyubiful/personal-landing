49cf8b7bd6aefa9f7ad8cf5a80531b230a1b4ce1
Kyubiful <ser.zabala@gmail.com>
2022-05-31 21:08:01 +0200

    🐛 fix: fix background color

--- src/components/OpinionCard/index.jsx
+++ src/components/OpinionCard/index.jsx
@@ -4,7 +4,7 @@ export const OpinionCard = ({ name, company, text, companyUrl }) => {
       <div className="absolute inset-0.5 group-hover:inset-0 dark:inset-1 dark:group-hover:inset-0.5 bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-300 opacity-50 group-hover:opacity-60 transition duration-900 rounded-xl blur mb-2">
       </div>
       <div className="transform h-full transition-all rounded-xl w-full p-2 mb-2">
-        <div className="h-full w-full bg-slate-100 dark:bg-slate-900 rounded-lg p-4">
+        <div className="h-full w-full bg-slate-100 bg-slate-900 rounded-lg p-4">
           <h3 className="text-m">{name}</h3>
           <h5 className="text-sm">
             <a data-testid="companyUrl" href={companyUrl}>{company}</a>
