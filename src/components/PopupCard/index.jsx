594c51f2bd8c123f0db741584fd2b9f43416171c
Kyubiful <ser.zabala@gmail.com>
2022-06-03 01:31:25 +0200

    ✨ feat: change status code

--- src/components/PopupCard/index.jsx
+++ src/components/PopupCard/index.jsx
@@ -1,7 +1,7 @@
 export const PopupCard = ({ status, message, className, active }) => {
   let cardClass = ''
 
-  if (status === 500) {
+  if (status === 200) {
     cardClass += 'bg-slate-600 '
   } else {
     cardClass += 'bg-red-600 '
