1e230ac53ae703353b0e73199e90ce697fa0935e
Kyubiful <ser.zabala@gmail.com>
2022-06-03 01:32:03 +0200

    ✨ feat: fix fetch and data

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -16,60 +16,57 @@ export const ContactForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault()
-    const data = Object.fromEntries(new FormData(e.target))
+    const formData = Object.fromEntries(new FormData(e.target))
 
     if (e.target.name.value.length === 0) {
       setData({ status: 400, msg: 'Error: El campo del nombre está vacío' })
       return null
     }
-    if (e.target.email.value.length === 0) {
+    if (e.target.from.value.length === 0) {
       setData({ status: 400, msg: 'Error: El campo del email está vacío' })
       return null
     }
-    if (e.target.email.value.length !== 0 && e.target.email.validity.valid === false) {
+    if (e.target.from.value.length !== 0 && e.target.from.validity.valid === false) {
       setData({ status: 400, msg: 'Error: Email incorrecto' })
       return null
     }
-    if (e.target.content.value.length === 0) {
+    if (e.target.text.value.length === 0) {
       setData({ status: 400, msg: 'Error: El campo del texto está vacío' })
       return null
     }
 
-    data.to = 'ser.zabala@gmail.com'
+    formData.to = 'ser.zabala@gmail.com'
 
     const options = {
-      method: 'POST',
+      method: 'post',
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origins': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(formData)
     }
 
-    fetch('https://kyubi-mailer.herokuapp.com/api/mail/send', options)
+    fetch('http://localhost:4000/api/mail/send', options)
       .then(res => res.json())
-      .then(data => setData(data))
-
-    console.log(data)
+      .then(data => {
+        setData(data)
+        if (data.status === 200) {
+          e.target.name.value = ''
+          e.target.from.value = ''
+          e.target.text.value = ''
+        }
+      })
 
     setTimeout(() => {
       setData({ status: 400, msg: 'Error' })
     }, 3000)
-
-    if (data.status === 500) {
-      e.target.name.value = ''
-      e.target.email.value = ''
-      e.target.content.value = ''
-    }
   }
 
   return (
     <>
       <form onSubmit={onSubmit} className="flex flex-col gap-2 mt-10" noValidate>
         <InputForm type="text" name="name" placeholder="Nombre" />
-        <InputForm type="email" name="email" placeholder="Email" />
-        <TextAreaForm name="content" placeholder="Texto" />
+        <InputForm type="email" name="from" placeholder="Email" />
+        <TextAreaForm name="text" placeholder="Texto" />
         <div className="flex justify-end">
           <InputForm type="submit" onClick={togglePopup} />
         </div>
