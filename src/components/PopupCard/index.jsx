bac9a385694d3f9ed97648ea37262cdb976f187a
Kyubiful <ser.zabala@gmail.com>
2022-06-03 00:03:39 +0200

    ✨ feat: create popupCard component

---
+++ src/components/PopupCard/index.jsx
@@ -0,0 +1,23 @@
+export const PopupCard = ({ status, message, className, active }) => {
+  let cardClass = ''
+
+  console.log(status)
+
+  if (status === 500) {
+    cardClass += 'bg-slate-600 '
+  } else {
+    cardClass += 'bg-red-600 '
+  }
+
+  if (active === true) {
+    cardClass += 'w-80 h-40 rounded-xl flex justify-center items-center transition opacity-0 fixed animate-popup ' + className
+  } else {
+    cardClass += 'w-80 h-40 rounded-xl flex justify-center items-center transition opacity-0 hidden ' + className
+  }
+
+  return (
+    <div className={cardClass}>
+      <p className="text-slate-200 w-64 text-center">{message}</p>
+    </div>
+  )
+}
