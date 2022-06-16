a1d14b36abcdbe60689c577bfa92e18417296f3e
Kyubiful <ser.zabala@gmail.com>
2022-06-16 13:36:00 +0200

    ✨ feat: change name for mobile menu and add logo

--- src/components/NavbarMobile/index.jsx
+++ src/components/NavbarMobile/index.jsx
@@ -4,6 +4,7 @@ import { JavaScriptLogo } from '../Logos/JavaScriptLogo.jsx'
 import { HTMLLogo } from '../Logos/HTMLLogo.jsx'
 import { TypeScriptLogo } from '../Logos/TypeScriptLogo.jsx'
 import { CSSLogo } from '../Logos/CSSLogo.jsx'
+import { SZLogo } from '../Logos/SZLogo.jsx'
 import { ChangeIdiom } from '../ChangeIdiom/index.jsx'
 import { useTranslation } from 'react-i18next'
 
@@ -17,7 +18,7 @@ export const NavbarMobile = ({ isActive, setMenuMobileIsOpen, menuMobileIsOpen, 
     <nav {...props} className={'fixed h-full bg-slate-800 z-40 right-0 left-auto ' + (isActive === false ? 'hidden' : 'block')}>
       <ul className="flex flex-col w-80 h-full">
         <div className="w-full h-20">
-          <p className="ml-4 h-full flex items-center">KyubiDev</p>
+          <p className="ml-4 h-full flex items-center"><SZLogo className="mr-4" circleColor="fill-slate-200" textColor="fill-slate-900" />Sergio Zabala</p>
         </div>
         <Tab onClick={closeMenuMobile} to="/">
           <ReactLogo className="fill-slate-200 h-4 w-auto mr-2"/>
